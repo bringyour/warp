01041c05f29dc14165ee3eaf4d8419e1f54222d0
Brien Colwell <xcolwell@gmail.com>
2023-06-26 10:50:21 -0700

    updates

--- config-updater/go.mod
+++ config-updater/go.mod
@@ -1,3 +1,8 @@
 module bringyour.com/warp/config-updater/v2
 
 go 1.20
+
+require (
+	github.com/coreos/go-semver v0.3.1 // indirect
+	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815 // indirect
+)
--- config-updater/main.go
+++ config-updater/main.go
@@ -1,63 +1,113 @@
 package main
 
+import (
+	"os"
+	"log"
+	"path/filepath"
+	"io/fs"
+	"errors"
+	"fmt"
 
-// if target dir exists, exit
-// if target dir does not exist, copy to target.tmp, then rename to target
+	"github.com/docopt/docopt-go"
+    "github.com/coreos/go-semver/semver"
+)
 
-// copy to /srv/warp/keys/
+
+const CONFIG_UPDATER_VERSION = "0.0.1"
+
+var Out *log.Logger
+var Err *log.Logger
+
+func init() {
+	Out = log.New(os.Stdout, "", log.Ldate | log.Ltime | log.Lshortfile)
+	Err = log.New(os.Stderr, "", log.Ldate | log.Ltime | log.Lshortfile)
+}
 
 
-// copy all dirs recrusively from /root/config/* to /srv/warp/config in a safe step:
-// if /srv/warp/config/<dir>, stop
-// if /srv/warp/config/<dir>.tmp exists, remove it
-// copy to /srv/warp/config/<dir>.tmp
-// mv /srv/warp/config/<dir>.tmp to /srv/warp/config/<dir>
+/*
+copy all dirs recursively from `/root/config/*` to `/srv/warp/config` in a safe sequence:
+1. if `/srv/warp/config/<dir>` exixts, stop
+2. if `/srv/warp/config/<dir>.tmp` exists, remove it
+3. copy to `/srv/warp/config/<dir>.tmp`
+4. mv `/srv/warp/config/<dir>.tmp` to `/srv/warp/config/<dir>`
+*/
 func main() {
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		panic(err)
-	}
-	entries, err := os.ReadDir(path.Join(homeDir, "config"))
+	usage := `Warp control. Fluid iteration and zero downtime continuous release.
+
+Usage:
+    config-updater <source_dir> <dest_dir>
+
+Options:
+    -h --help                  Show this screen.
+    --version                  Show version.`
+
+    opts, err := docopt.ParseArgs(usage, os.Args[1:], CONFIG_UPDATER_VERSION)
+    if err != nil {
+        panic(err)
+    }
+
+	sourceDir, _ := opts.String("<source_dir>")
+	destDir, _ := opts.String("<dest_dir>")
+
+	entries, err := os.ReadDir(sourceDir)
 	if err != nil {
 		panic(err)
 	}
 
-	rootPaths := map[string]string{}
+	rootPaths := map[*semver.Version]string{}
 	for _, entry := range entries {
 		if entry.IsDir() {
-			version := entry.Name()
-			rootPaths[version] = path.Join(homeDir, "config", version)
+			versionStr := entry.Name()
+			if version, err := semver.NewVersion(versionStr); err == nil {
+				rootPaths[version] = filepath.Join(sourceDir, versionStr)
+			} else {
+				Err.Printf("Ignoring non-version dir (%s)\n", versionStr)
+			}
 		}
 	}
 
 	for version, rootPath := range rootPaths {
-		targetRootPath := path.Join("/srv/warp/config", version)
-		if EXISTS(targetRootPath) {
-			Err.Printf("Target path already exists. Will not copy. (%s)", targetRootPath)
+		targetRootPath := filepath.Join(destDir, version.String())
+		if _, err := os.Stat(targetRootPath); !errors.Is(err, os.ErrNotExist) {
+			Err.Printf("Target path already exists. Will not copy. (%s)\n", targetRootPath)
 			continue
 		}
-		targetTempRootPath := path.Join("/srv/warp/config", fmt.Sprintf("%s.tmp", version))
-		if EXISTS(targetTempRootPath) {
-			RM(targetTempRootPath)
+		targetTempRootPath := filepath.Join(destDir, fmt.Sprintf("%s.tmp", version.String()))
+		if _, err := os.Stat(targetTempRootPath); !errors.Is(err, os.ErrNotExist) {
+			Err.Printf("Removing existing partial directory. (%s)\n", targetTempRootPath)
+			os.RemoveAll(targetTempRootPath)
 		}
+		Err.Printf("Copy %s -> %s\n", rootPath, targetTempRootPath)
 		err := copy(rootPath, targetTempRootPath)
 		if err != nil {
-			Err.Printf("Error during copy. Version %s will not be deployed. %s", version, err)
+			Err.Printf("Error during copy. Version %s will not be deployed. (%s)\n", version.String(), err)
 			continue
 		}
-		err = move(targetTempRootPath, targetRootPath)
+		Err.Printf("Move %s -> %s\n", targetTempRootPath, targetRootPath)
+		err = os.Rename(targetTempRootPath, targetRootPath)
 		if err != nil {
-			Err.Printf("Error during move. Version %s will not be deployed. %s", version, err)
+			Err.Printf("Error during move. Version %s will not be deployed. (%s)\n", version.String(), err)
 		}
 	}
 }
 
 func copy(sourceRootPath string, targetRootPath string) error {
-	// FIXME
+	filepath.Walk(sourceRootPath, func(path string, info fs.FileInfo, err error)(error) {
+		relPath, _ := filepath.Rel(sourceRootPath, path)
+		targetPath := filepath.Join(targetRootPath, relPath)
+		if info.IsDir() {
+			os.Mkdir(targetPath, info.Mode())
+		} else {
+			data, err := os.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			os.WriteFile(targetPath, data, info.Mode())
+		}
+		return nil
+	})
+	return nil
 }
 
-func move(sourceRootPath string, targetRootPath string) error {
-	// FIXME
-}
 
 
